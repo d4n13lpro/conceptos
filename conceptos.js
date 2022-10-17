1b542b21828ef1a821d67ebc2f2ede218a0ce68c
d4n13lpro <cdap785@gmail.com>
2022-10-17 18:02:47 -0500

    mostrando cambios corto

---
+++ conceptos.js
@@ -0,0 +1,12 @@
+// Que es Git?
+// Es un sistema de control de versiones 
+//Registra los cambios realizados a un archivo o conjunto de archivos a lo 
+//largo del tiempo de modo que puedas recuperar versiones especificas mas
+//adelante
+
+// Que es Git-Up?
+// Es una plataforma para almacenar tus repositorios (un repositorio de 
+// repositorios)
+// Es un lugar donde puedo compartir mi codigo o encontrar otros proyectos
+// jdjdjd
+un chanchito feliz
